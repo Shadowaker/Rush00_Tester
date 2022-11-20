e05f14533c9129f8cd2e988e729119967a76dda2
Giorgia Cucino <gcucino@student.42roma.it>
2022-11-20 18:06:25 +0100

    facciamo il culo agli arroganti

--- rush/ft_putchar.c
+++ rush/ft_putchar.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dridolfo <dridolfo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gcucino <gcucino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 17:14:07 by dridolfo          #+#    #+#             */
-/*   Updated: 2022/09/11 22:38:25 by dridolfo         ###   ########.fr       */
+/*   Updated: 2022/11/20 17:47:42 by gcucino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@ int	ft_putchar(int c)
 
 void	writer(int i, int j, int *coord, char *el)
 {
-	if (i == 0 && j == 0)
-		write(1, &el[0], 1);
-	else if (i == 0 && j == (coord[0] - 1))
-		write(1, &el[1], 1);
-	else if (i == (coord[1] - 1) && j == 0)
-		write(1, &el[2], 1);
-	else if (i == (coord[1] - 1) && j == (coord[0] - 1))
-		write(1, &el[3], 1);
-	else if (i == 0 || i == (coord[1] - 1))
-		write(1, &el[4], 1);
-	else if (j == 0 || j == (coord[0] - 1))
-		ft_putchar(el[5]);
-	else
-		write(1, " ", 1);
+	// if (i == 0 && j == 0)
+	// 	write(1, &el[0], 1);
+	// else if (i == 0 && j == (coord[0] - 1))
+	// 	write(1, &el[1], 1);
+	// else if (i == (coord[1] - 1) && j == 0)
+	// 	write(1, &el[2], 1);
+	// else if (i == (coord[1] - 1) && j == (coord[0] - 1))
+	// 	write(1, &el[3], 1);
+	// else if (i == 0 || i == (coord[1] - 1))
+	// 	write(1, &el[4], 1);
+	// else if (j == 0 || j == (coord[0] - 1))
+	// 	ft_putchar(el[5]);
+	// else
+	// 	write(1, " ", 1);
+	;
 }
 
 void	rush_(int *coord, char *el)
--- rush/main.c
+++ rush/main.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dridolfo <dridolfo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gcucino <gcucino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 21:49:11 by dridolfo          #+#    #+#             */
-/*   Updated: 2022/09/11 22:38:45 by dridolfo         ###   ########.fr       */
+/*   Updated: 2022/11/20 18:01:53 by gcucino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int	rush(int x, int y);
 
@@ -34,6 +35,27 @@ static int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
 
+int	ft_check_int(const char *str, int i, int s)
+{
+	int	j;
+
+	j = 0;
+	while (str[i + j] != '\0' && (str[i + j] > 47 && str[i + j] < 58))
+		j++;
+	printf("%d\n", j);
+	if (j > 10)
+		return (-1);
+	else if (j < 10)
+		return (1);
+	else
+	{
+		if (s == 1)
+			return (ft_strncmp(&str[i], "2147483647", 10) <= 0);
+		else
+			return (ft_strncmp(&str[i], "2147483648", 10) <= 0);
+	}
+}
+
 static int	ft_atoi(const char *str)
 {
 	int		i;
@@ -43,8 +65,6 @@ static int	ft_atoi(const char *str)
 	i = 0;
 	r = 0;
 	s = 1;
-	if (ft_strncmp("-2147483648", str, 12) == 0)
-		return (-2147483648);
 	while (str[i] != '\0' && ((str[i] > 8 && 14 > str[i]) || str[i] == 32))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
@@ -53,6 +73,8 @@ static int	ft_atoi(const char *str)
 			s *= -1;
 		i++;
 	}
+	if (ft_check_int(str, i, s) == 0)
+		return (-1);
 	while (str[i] != '\0' && (str[i] > 47 && str[i] < 58))
 	{
 		r *= 10;
@@ -64,8 +86,19 @@ static int	ft_atoi(const char *str)
 
 int	main(int argc, char **argv)
 {
+	int	x;
+	int	y;
+	
 	if (argc != 3)
-		write(1, "Error: Wrong number of arguments\n", 34);
-	rush(ft_atoi(argv[1]), ft_atoi(argv[2]));
+		return(write(1, "Error: Wrong number of arguments\n", 34));
+	x = ft_atoi(argv[1]);
+	y = ft_atoi(argv[2]);
+	printf("%d, %d\n", x, y);
+	if (x < 0 || y < 0)
+	{
+		write(1, "Invalid arguments passed.\n", 27);
+		return(-1);
+	}
+	// rush(x, y);
 	return (0);
 }
